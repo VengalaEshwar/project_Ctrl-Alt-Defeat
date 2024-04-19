91cb85a54a20e393baf229613329bb877ba12ce2
Eshwar <eshwarvengala30@gmail.com>
2024-04-20 01:44:33 +0530

    Done with the 50% of thr project

--- ml-model/temp.py
+++ ml-model/temp.py
@@ -1,75 +1,34 @@
-import numpy as np
-import pandas as pd
-import sklearn
-import matplotlib
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import BaggingClassifier
-from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import accuracy_score
-
-data=pd.read_csv("kidney_disease.csv")
-
-X = data.drop('classification', axis=1)  
-y = data['classification']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-""" X_train_np = X_train.to_numpy()
-y_train_np = y_train.to_numpy()
-
-model = RandomForestClassifier()
-model.fit(X_train_np, y_train_np) """
-from sklearn.preprocessing import LabelEncoder
-model = RandomForestClassifier()
-# Encode y_train
-label_encoder = LabelEncoder()
-y_train = label_encoder.fit_transform(y_train)
-
-# Now, y_train should be ready for training
-model.fit(X_train, y_train)
-
-"""
-# Make predictions
-y_pred = model.predict(X_test)
-
-# Evaluate accuracy
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
- """
-""" models = {
-    'Logistic Regression': LogisticRegression(),
-    'Random Forest': RandomForestClassifier(),
-    'SVM': SVC(),
-    'KNN': KNeighborsClassifier(),
-    'Naive Bayes': GaussianNB(),
-    'Neural Network': MLPClassifier(),
-    'AdaBoost': AdaBoostClassifier(),
-    'Gradient Boosting': GradientBoostingClassifier(),
-    'Extra Trees': ExtraTreesClassifier(),
-    'Decision Tree': DecisionTreeClassifier(),
-    'Bagging': BaggingClassifier(),
-    'SGD': SGDClassifier()
+import DataPreProcessing as pre
+data = {
+     'id': 6,
+    'age': 68.0,
+    'bp': 70.0,
+    'sg': 1.01,
+    'al': 0.0,
+    'su': 0.0,
+    'rbc': None,
+    'pc': 'normal',
+    'pcc': 'notpresent',
+    'ba': 'notpresent',
+    'bgr': 100.0,
+    'bu': 54.0,
+    'sc': 24.0,
+    'sod': 104.0,
+    'pot': 4.0,
+    'hemo': 12.4,
+    'pcv': 36,
+    'wc': None,
+    'rc': None,
+    'htn': 'no',
+    'dm': 'no',
+    'cad': 'no',
+    'appet': 'good',
+    'pe': 'no',
+    'ane': 'no',
+    'classification': 'ckd'
 }
- """
-# Train and evaluate each model
-""" accuracies = {}
-for name, model in models.items():
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    accuracies[name] = accuracy
-    print(f'{name} - Accuracy: {accuracy}') """
-"""
-# Compare the accuracies
-best_model = max(accuracies, key=accuracies.get)
-print(f'\nBest Model: {best_model} - Accuracy: {accuracies[best_model]}')
- """
+
+data1 = {key: [value] for key, value in data.items()}
+# print(data1)
+print(pre.preprocess(data1))
+print(data1)
