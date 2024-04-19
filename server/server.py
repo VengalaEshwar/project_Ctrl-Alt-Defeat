141302f07eed0c822e25bd358a9ffd83be7e37da
Eshwar <eshwarvengala30@gmail.com>
2024-04-19 17:24:33 +0530

    Done with implementation of front end 1

--- server/server.py
+++ server/server.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    data["some"] =[1,2,3,4]
+    # data["some"] =[1,2,3,4]
     prediction =  data #your_ml_module.predict(data)  # Call your machine learning model function
     print("Handled the data")
+    print(data)
     return jsonify(prediction)
 
 if __name__ == '__main__':
